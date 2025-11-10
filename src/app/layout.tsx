853227db6489a5c605a4a5c48aefdfeb6a47823a
lianxiao.nm <lianxiao.nm@gmail.com>
2025-11-10 18:00:36 +0800

    feat: add fetch api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,20 @@ import { fetchFooter, fetchHeader } from "@/api/common";
 import Layout from "@/components/Layout";
 import "../global.css";
 
+import header from "../../public/api/header.json";
+
 export const metadata = { title: "My Next App" };
 
 type Props = { children: ReactNode };
 
 export default async function RootLayout({ children }: Props) {
-  const [header, footer] = await Promise.all([fetchHeader(), fetchFooter()]);
+  const [header, footer] = await Promise.all([
+    require("../../public/api/header.json"),
+    require("../../public/api/footer.json"),
+    // fetchHeader(),
+    // fetchFooter(),
+  ]);
+  console.log("layout", header, footer);
 
   return (
     <html lang="en">
